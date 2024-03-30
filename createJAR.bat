055c1f1a24310c39204d0fe0a590eeea1317e960
mittko <d.alexandrov3@gmail.com>
2024-03-30 22:22:07 +0200

    AddedBatchFilesForGeneratingAndRunningSpringBootJarWithSpecificJavaVersion/AddedEndpointForShutdownApplication(Tomacat)

---
+++ createJAR.bat
@@ -0,0 +1,8 @@
+Change your code page to UTF-8 with chcp 65001 in your batch file or CMD session.
+chcp 65001
+
+REM make mvn to use speciffic java version
+set JAVA_HOME=C:\Users\ПОЖАРПРОТЕКТ\.jdks\corretto-17.0.8.1
+
+REM create jar file
+mvn package
---
+++ runCreateJar.bat
@@ -0,0 +1,5 @@
+REM   1. go to app directory
+cd D:\PojarprotektAPI
+
+REM   2. start createJar.bat
+start createJar.bat
