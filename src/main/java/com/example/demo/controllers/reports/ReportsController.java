8395f049d849a6b32bb16384235e2a72f754d277
mittko <d.alexandrov3@gmail.com>
2024-07-14 19:55:28 +0300

    AddedProtokolsReportsEndPoint

--- src/main/java/com/example/demo/controllers/reports/ReportsController.java
+++ src/main/java/com/example/demo/controllers/reports/ReportsController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers.reports;
 
 import com.example.demo.callbacks.ResultSetCallback;
+import com.example.demo.models.ProtokolReports;
 import com.example.demo.models.ServiceOrderReports;
 import com.example.demo.services.RepoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,105 @@ public class ReportsController<T> {
 
         String command = "select client, type, wheight, barcod, serial, category, brand," +
                 " T_O, P, HI, done, number, person, date , additional_data from ServiceTableDB";
+
+
+        command += constructQueryWithPrams(client,number,type,wheight,category,brand,doing,
+                serial,barcode,fromDate,toDate);
+
+        repoService.getResult(command, new ResultSetCallback() {
+            @Override
+            public void result(ResultSet resultSet) throws SQLException {
+                while (resultSet.next()) {
+                    ServiceOrderReports<T> serviceOrderReports = new ServiceOrderReports<T>();
+                    serviceOrderReports.setClient(resultSet.getString(1));
+                    serviceOrderReports.setType(resultSet.getString(2));
+                    serviceOrderReports.setWheight(resultSet.getString(3));
+                    serviceOrderReports.setBarcod(resultSet.getString(4));
+                    serviceOrderReports.setSerial(resultSet.getString(5));
+                    serviceOrderReports.setCategory(resultSet.getString(6));
+                    serviceOrderReports.setBrand(resultSet.getString(7));
+                    serviceOrderReports.setT_O(resultSet.getString(8));
+                    serviceOrderReports.setP(resultSet.getString(9));
+                    serviceOrderReports.setHI(resultSet.getString(10));
+                    serviceOrderReports.setDone(resultSet.getString(11));
+                    serviceOrderReports.setNumber(resultSet.getString(12));
+                    serviceOrderReports.setPerson(resultSet.getString(13));
+                    serviceOrderReports.setDate(resultSet.getString(14));
+                    serviceOrderReports.setAdditional_data(resultSet.getString(15));
+                    serviceOrders.add((T) serviceOrderReports);
+                }
+            }
+        });
+        return serviceOrders;
+    }
+
+
+    @GetMapping(path = "/protokols")
+    public @ResponseBody ArrayList<T> getProtokols(
+            @RequestParam(value = "client",required = false) String client,
+            @RequestParam(value = "type", required = false) String type,
+            @RequestParam(value = "wheight", required = false) String wheight,
+            @RequestParam(value = "barcod", required = false) String barcod,
+            @RequestParam(value = "serial", required = false) String serial,
+            @RequestParam(value = "category", required = false) String category,
+            @RequestParam(value = "brand", required = false) String brand,
+            @RequestParam(value = "doing", required = false) String doing,
+            @RequestParam(value = "number", required = false) String number,
+            @RequestParam(value = "fromDate", required = false) String fromDate,
+            @RequestParam(value = "toDate", required = false) String toDate) throws SQLException {
+
+        ArrayList<T> protokolReportList = new ArrayList<>();
+
+        String command = "select client, type, wheight, barcod, serial, category, brand, " +
+                "T_O, P, HI, parts, value, number, person, date, kontragent," +
+                "invoiceByKontragent, additional_data, uptodate from ProtokolTableDB5";
+
+        command += constructQueryWithPrams(client,number,type,wheight,category,brand,doing,
+                serial,barcod, fromDate,toDate);
+        repoService.getResult(command, new ResultSetCallback() {
+            @Override
+            public void result(ResultSet resultSet) throws SQLException {
+               while (resultSet.next()) {
+                   ProtokolReports<T> protokolReports = new ProtokolReports<T>();
+                   protokolReports.setClient(resultSet.getString(1));
+                   protokolReports.setType(resultSet.getString(2));
+                   protokolReports.setWheight(resultSet.getString(3));
+                   protokolReports.setBarcod(resultSet.getString(4));
+                   protokolReports.setSerial(resultSet.getString(5));
+                   protokolReports.setCategory(resultSet.getString(6));
+                   protokolReports.setBrand(resultSet.getString(7));
+                   protokolReports.setT_O(resultSet.getString(8));
+                   protokolReports.setP(resultSet.getString(9));
+                   protokolReports.setHI(resultSet.getString(10));
+                   protokolReports.setParts(resultSet.getString(11));
+                   protokolReports.setValue(resultSet.getString(12));
+                   protokolReports.setNumber(resultSet.getString(13));
+                   protokolReports.setPerson(resultSet.getString(14));
+                   protokolReports.setDate(resultSet.getString(15));
+                   protokolReports.setKontragent(resultSet.getString(16));
+                   protokolReports.setInvoiceByKontragent(resultSet.getString(17));
+                   protokolReports.setAdditional_data(resultSet.getString(18));
+                   protokolReports.setUptodate(resultSet.getString(19));
+
+                   protokolReportList.add((T) protokolReports);
+               }
+            }
+        });
+        return protokolReportList;
+    }
+
+    private String constructQueryWithPrams(String client,
+                                          String number,
+                                         String type,
+                                         String wheight,
+                                         String category,
+                                         String brand,
+                                         String doing,
+                                         String serial,
+                                         String barcode,
+                                         String fromDate,
+                                         String toDate) {
+        String command = "";
         int selectedCriterii = 0;
         if(client != null) {
             command += String.format(" where client = '%s'",client);// Димитър Александров
@@ -101,38 +201,14 @@ public class ReportsController<T> {
             command += String.format(" %s barcod = '%s'",placeholder,barcode); // 1000018166032
             selectedCriterii++;
         }
+
         if(fromDate != null & toDate != null) {
             String placeholder = null;
             placeholder = (selectedCriterii == 0) ? "where" :  "and";
-            command += String.format(" %s date between " + "Date('%s') and Date('%s')",placeholder,fromDate,toDate); // 1000018166032
+            command += String.format(" %s date between Date('%s') and Date('%s')",placeholder,fromDate,toDate); // 1000018166032
         }
-        repoService.getResult(command, new ResultSetCallback() {
-            @Override
-            public void result(ResultSet resultSet) throws SQLException {
-                while (resultSet.next()) {
-                    ServiceOrderReports<T> serviceOrderReports = new ServiceOrderReports<T>();
-                    serviceOrderReports.setClient(resultSet.getString(1));
-                    serviceOrderReports.setType(resultSet.getString(2));
-                    serviceOrderReports.setWheight(resultSet.getString(3));
-                    serviceOrderReports.setBarcod(resultSet.getString(4));
-                    serviceOrderReports.setSerial(resultSet.getString(5));
-                    serviceOrderReports.setCategory(resultSet.getString(6));
-                    serviceOrderReports.setBrand(resultSet.getString(7));
-                    serviceOrderReports.setT_O(resultSet.getString(8));
-                    serviceOrderReports.setP(resultSet.getString(9));
-                    serviceOrderReports.setHI(resultSet.getString(10));
-                    serviceOrderReports.setDone(resultSet.getString(11));
-                    serviceOrderReports.setNumber(resultSet.getString(12));
-                    serviceOrderReports.setPerson(resultSet.getString(13));
-                    serviceOrderReports.setDate(resultSet.getString(14));
-                    serviceOrderReports.setAdditional_data(resultSet.getString(15));
-                    serviceOrders.add((T) serviceOrderReports);
-                }
-            }
-        });
-        return serviceOrders;
+
+        command += " order by CAST(date as DATE) desc";
+        return command;
     }
-
-
-
 }
---
+++ src/main/java/com/example/demo/models/ProtokolReports.java
@@ -0,0 +1,175 @@
+package com.example.demo.models;
+
+public class ProtokolReports<T> {
+    private String client;
+    private String type;
+    private String wheight;
+    private String barcod;
+    private String serial;
+    private String category;
+    private String brand;
+    private String  T_O;
+    private String P;
+    private String HI;
+    private String parts;
+    private String value;
+    private String number;
+    private String person;
+    private String date;
+    private String kontragent;
+    private String invoiceByKontragent;
+    private String additional_data;
+    private String uptodate;
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWheight() {
+        return wheight;
+    }
+
+    public void setWheight(String wheight) {
+        this.wheight = wheight;
+    }
+
+    public String getBarcod() {
+        return barcod;
+    }
+
+    public void setBarcod(String barcod) {
+        this.barcod = barcod;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getT_O() {
+        return T_O;
+    }
+
+    public void setT_O(String t_O) {
+        T_O = t_O;
+    }
+
+    public String getP() {
+        return P;
+    }
+
+    public void setP(String p) {
+        P = p;
+    }
+
+    public String getHI() {
+        return HI;
+    }
+
+    public void setHI(String HI) {
+        this.HI = HI;
+    }
+
+    public String getParts() {
+        return parts;
+    }
+
+    public void setParts(String parts) {
+        this.parts = parts;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getKontragent() {
+        return kontragent;
+    }
+
+    public void setKontragent(String kontragent) {
+        this.kontragent = kontragent;
+    }
+
+    public String getInvoiceByKontragent() {
+        return invoiceByKontragent;
+    }
+
+    public void setInvoiceByKontragent(String invoiceByKontragent) {
+        this.invoiceByKontragent = invoiceByKontragent;
+    }
+
+    public String getAdditional_data() {
+        return additional_data;
+    }
+
+    public void setAdditional_data(String additional_data) {
+        this.additional_data = additional_data;
+    }
+
+    public String getUptodate() {
+        return uptodate;
+    }
+
+    public void setUptodate(String uptodate) {
+        this.uptodate = uptodate;
+    }
+}
